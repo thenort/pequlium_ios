e7026f28403750ee727e67492e378952af1c268b
Kyrylo Matvieiev <kyrylo.matvieiev@gmail.com>
2017-03-17 17:04:12 +0200

    Commit 17/3/17

---
+++ Pequlium/Pequlium/NetViewController.swift
@@ -0,0 +1,28 @@
+//
+//  NetViewController.swift
+//  Pequlium
+//
+//  Created by Kyrylo Matvieiev on 17/03/2017.
+//  Copyright © 2017 Kyrylo Matvieiev. All rights reserved.
+//
+
+import UIKit
+
+class NetViewController: UIViewController {
+
+    @IBOutlet weak var appStoreWebView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let url = URL(string: "https://itunes.apple.com/us/app/pequlium/id1216665284?ls=1&mt=8") {
+            let request = URLRequest(url: url)
+            self.appStoreWebView.loadRequest(request)
+        }
+    }
+    
+    deinit {
+        print("call method NetViewController deinit")
+    }
+    
+}
